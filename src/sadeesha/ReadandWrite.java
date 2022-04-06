e170fa8963bac435695408c3493f7fb1e7ff20ac
SadeeshaNH87 <58145312+SadeeshaNH87@users.noreply.github.com>
2022-04-06 18:47:14 +0530

    Writing user inputs to a file

--- src/sadeesha/ReadandWrite.java
+++ src/sadeesha/ReadandWrite.java
@@ -1,4 +1,30 @@
 package sadeesha;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class ReadandWrite {
+    public static void main(String[] args) {
+
+        //region - taking user input
+        Scanner userTextScan = new Scanner(System.in);
+        System.out.println("Enter the desired text: ");
+
+        String text = userTextScan.nextLine();
+        //endregion
+
+        //region - writing to a file
+        //this File Writer throws a IO exception. Hence, we are handling it using a try catch block
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output_file.txt"));
+            writer.write(text);
+            writer.close(); // #---- Always make sure to close the file. Otherwise, nothing will be written into file ----#
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //endregion
+    }
 }
